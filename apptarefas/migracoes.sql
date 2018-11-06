7ef88243359162eadf313d0168d6ae494078aeea
fredericogon <fredericogon@gmail.com>
2018-11-06 15:10:05 -0200

    adicionado criacao da tabela usuarios no arquivo migracoes

--- apptarefas/migracoes.sql
+++ apptarefas/migracoes.sql
@@ -1,1 +1,24 @@
 --migrações sql
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+create table usuarios(
+	id serial not null primary key,
+	nome varchar(120) not null,
+    email varchar(120) not null,
+    senha varchar (20) not null,
+    "dataHoraCriacao" timestamp not null,
+    "dataHoraAtualizacao" timestamp not null,
+    status varchar(100) not null
+);
