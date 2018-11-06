18b55881333fd127fd1d169bcfd2115a3678ad21
Eduardo Buchele <eduardobuchele@gmail.com>
2018-11-06 15:24:58 -0200

    Adicionando tabela situacao no arquivo migracoes

--- apptarefas/migracoes.sql
+++ apptarefas/migracoes.sql
@@ -1,18 +1,17 @@
 --migrações sql
-<<<<<<< HEAD
 
+create database "transpoBrasil";
 
-
-
-
-
-
-
-
-
-
-
-
+--tabela projeto
+create table projetos (
+    id serial not null primary key,
+    nome varchar(100) not null,
+    descricao varchar(1000) not null,
+    status varchar(100),
+    "dataHoraCriacao" timestamp not null,
+    "dataHoraAtualizacao" timestamp not null,
+    "dataHoraFim" timestamp not null
+);
 
 create table usuarios(
 	id serial not null primary key,
@@ -23,19 +22,20 @@ create table usuarios(
     "dataHoraAtualizacao" timestamp not null,
     status varchar(100) not null
 );
-=======
-create database "transpoBrasil";
 
-create table projetos (
-    id serial not null primary key,
-    nome varchar(100) not null,
-    descricao varchar(1000) not null,
-    status varchar(100),
-    "dataHoraCriacao" timestamp not null,
-    "dataHoraAtualizacao" timestamp not null,
-    "dataHoraFim" timestamp not null
+
+
+
+
+
+create table situacao(
+	id serial not null primary key,
+	nome varchar(120) not null,
+	tipo varchar(120) not null, 
+	"dataHoraCriacao" timestamp not null, 
+	"dataHoraAtualizacao" timestamp not null,
+	"dataHoraRemocao" timestamp,
+	status varchar(100) not null
 );
---tabela projeto
---tabela projeto
---tabela projeto
->>>>>>> 6e0badc1b53cf4c021770deb95381fdef76cb6ec
+
+
