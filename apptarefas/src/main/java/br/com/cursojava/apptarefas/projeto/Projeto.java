2df41076eb8eee44493f497dd5475ab6018709e5
anthueeccel <anthue@gmail.com>
2018-11-06 16:50:38 -0200

    merge .classpath

--- apptarefas/src/main/java/br/com/cursojava/apptarefas/projeto/Projeto.java
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/projeto/Projeto.java
@@ -102,13 +102,6 @@ public class Projeto {
 	}
 
 	@Override
-	public String toString() {
-		return "Projeto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", status=" + status
-				+ ", DataHoraCriacao=" + DataHoraCriacao + ", DataHoraAtualizacao=" + DataHoraAtualizacao
-				+ ", DataHoraFim=" + DataHoraFim + "]";
-	}
-
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -131,6 +124,13 @@ public class Projeto {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Projeto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", status=" + status
+				+ ", DataHoraCriacao=" + DataHoraCriacao + ", DataHoraAtualizacao=" + DataHoraAtualizacao
+				+ ", DataHoraFim=" + DataHoraFim + "]";
 	}
 
 }
