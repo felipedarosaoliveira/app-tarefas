d530ef56f9093d014e5ebe81067de09588d98d20
fredericogon <fredericogon@gmail.com>
2018-11-06 16:41:23 -0200

    craiacao da interface de repositorio e classe de implementacao para situacao

---
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/projeto/CrudRepository.java
@@ -0,0 +1,14 @@
+package br.com.cursojava.apptarefas.projeto;
+
+import java.util.List;
+
+public interface CrudRepository<T> {
+	
+	public boolean inserir(Class<T> classe);
+	public boolean atualizar(T t);
+	public boolean remover(T t);
+	public List<T> buscarTodos();
+	public T buscarPorId(int id);
+	public long contar();
+
+}
