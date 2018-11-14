dd2851e41e21b6fcabf3c217fa685279f3e63107
Nicole <nicole.gianisini.dietrich@gmail.com>
2018-11-14 16:53:13 -0200

    outras alterações tarefa facade

--- apptarefas/src/main/java/br/com/cursojava/apptarefas/tarefa/TarefaFacade.java
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/tarefa/TarefaFacade.java
@@ -2,13 +2,10 @@ package br.com.cursojava.apptarefas.tarefa;
 
 import java.util.List;
 
-import br.com.cursojava.apptarefas.projeto.Projeto;
-import br.com.cursojava.apptarefas.situacao.Situacao;
-import br.com.cursojava.apptarefas.usuario.Usuario;
-
 public class TarefaFacade {
 	
-	private TarefaRepositorio repositorio = new TarefaRepositorio();
+
+private TarefaRepositorio repositorio = new TarefaRepositorio();
 	
 	
 	public Tarefa novaTarefa() {
@@ -23,36 +20,25 @@ public class TarefaFacade {
 		return repositorio.buscarTodos();
 	}
 	
-	public boolean salvarTarefa(Tarefa tarefaAtual) {
-		return repositorio.salvar(tarefaAtual);
-		
+	public boolean salvar(Tarefa tarefaAtual) {
+		boolean resultado = false;
+		if(tarefaAtual.getId() == null) {
+			resultado = repositorio.inserir(tarefaAtual);
+			System.out.println("Inserindo tarefa no Banco: " + tarefaAtual.getNome());
+		}else {
+			resultado = repositorio.atualizar(tarefaAtual);
+			System.out.println("Atualizando tarefa no Banco: " + tarefaAtual.getNome());
+		}
+		return resultado;
 	}
 	public boolean removerTarefa(Tarefa tarefaAtual) {
 		return repositorio.remover(tarefaAtual.getId());
-
-	}
-	
-	public boolean editarTarefa(Tarefa tarefaAtual) {
-		return repositorio.editar(tarefaAtual);
 	}
 	
 	public List<Tarefa> buscarSituacaoTarefa(String situacao) {
 		return repositorio.buscarPorSituacao(situacao);
 	}
 
-	public List<Projeto> carregarProjetos() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	public List<Usuario> carregarUsuarios() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<Situacao> carregarSituacao() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
 
