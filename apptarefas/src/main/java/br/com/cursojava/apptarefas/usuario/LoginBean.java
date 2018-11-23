454583a901e6e87ae21bfc545b1b05a37e5772f7
anthueeccel <anthue@gmail.com>
2018-11-23 15:14:51 -0200

    implementado LoginBean

--- apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/LoginBean.java
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/LoginBean.java
@@ -1,33 +1,52 @@
 package br.com.cursojava.apptarefas.usuario;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 
-import br.com.cursojava.apptarefas.utils.Sistema;
+import br.com.cursojava.apptarefas.utils.AppSession;
+import br.com.cursojava.apptarefas.utils.ValidationResult;
 
-
+@ManagedBean
+@ViewScoped
 public class LoginBean {
 
 	private String email;
 	private String senha;
-	Usuario usuario = new Usuario();
+	private UsuarioFacade facade = new UsuarioFacade();
+	private AppSession session;
 
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
 	public String autenticar() {
-		FacesContext ctx = FacesContext.getCurrentInstance();
-		if (usuario.getEmail().equals(email) && usuario.getSenha().equals(Sistema.gerarHash(senha))) {
-			HttpSession session = (HttpSession) ctx.getExternalContext().getSession(true);
-			session.setAttribute("usuarioLogado", usuario);
+		FacesContext ctx = FacesContext.getCurrentInstance();		
+		ValidationResult result = facade.autenticar(email,senha, session);
+		if(result.isOk()) {			
 			return "index.xhtml?faces-redirect=true";
-		} else {
-
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Inválido",
+		}else{
+				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Inválido",
 					"Email ou senha inválidos");
-
 			ctx.addMessage(null, msg);
+			return null;
 		}
-		return null;
 	}
 	
-
+	
 }
--- apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/UsuarioFacade.java
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/UsuarioFacade.java
@@ -3,12 +3,13 @@ package br.com.cursojava.apptarefas.usuario;
 import java.util.Date;
 import java.util.List;
 
+import br.com.cursojava.apptarefas.utils.AppSession;
 import br.com.cursojava.apptarefas.utils.ValidationResult;
 
 public class UsuarioFacade {
 
 	private UsuarioRepositorio repositorio = new UsuarioRepositorio();
-	private UsuarioBusiness business  = new UsuarioBusiness();
+	private UsuarioBusiness business = new UsuarioBusiness();
 
 	public Usuario novoUsuario() {
 		return new Usuario();
@@ -22,13 +23,13 @@ public class UsuarioFacade {
 		boolean ok = false;
 		usuarioAtual.setDataHoraAtualizacao(new Date());
 		ValidationResult result = business.validar(usuarioAtual);
-		if(result.isOk()){
+		if (result.isOk()) {
 			if (usuarioAtual.getId() == null) {
 				ok = repositorio.inserir(usuarioAtual);
 			} else {
 				ok = repositorio.atualizar(usuarioAtual);
 			}
-			if(!ok){
+			if (!ok) {
 				result.addErrorMessage("persistencia", "Não foi possível salvar os dados do usuário.");
 			}
 		}
@@ -41,5 +42,14 @@ public class UsuarioFacade {
 
 	public List<Usuario> carregarUsuarios() {
 		return repositorio.buscarTodos();
+	}
+
+	public ValidationResult autenticar(String email, String senha,AppSession session) {
+		Usuario usuario = repositorio.buscarPorEmail(email);
+		 ValidationResult autenticar = business.autenticar(usuario, senha);
+		 if(autenticar.isOk()) {
+			 session.addAttribute("usuarioLogado", usuario);
+		 }
+		 return autenticar;
 	}
 }
