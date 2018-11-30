af1e5d88e301cc13bf39c38bf26cc56347e202cd
AnthueEccel <39606940+anthueeccel@users.noreply.github.com>
2018-11-30 10:19:35 -0200

    add C3-PO  (#129)
    * teste arquivo teste-branch.txt
    
    * merge master
    
    * add footer.xhtml
    
    * add footer.xhtml colors
    
    * add TarefasBean (incompleto)
    
    * implementação TarefasBean (incompleta) add util/Systema.java
    
    * atualizado TarefaBean
    
    * a
    
    * implementado PrimeFaces situação lista
    
    * implementado contadores TarefaBean
    
    * ímplementado TarefaBean extends AbstractBean
    
    * implmentado TarefaBean e TarefaFaceda add carregarUsuarios, carregarSituacoes, carregarProjetos
    
    * add template-form.chtml / atualizado projeto/formulario.xhtml
    
    * corregido TarefaBean
    
    * update
    
    * update situacao/list com template
    
    * ProjetoBean update Validation
    
    * correcão id projeto/lista
    
    * corrigido usuario/formulario
    
    * update
    
    * implementado LoginBean
    
    * implementado LoginBean gerarHash. atualizado Migrações.sql add t@teste.com senha 12345678 (cripto)
    
    * implementado LoginBean e tela de login (valida usuario)
    
    * add web.xml param fontawesome
    
    * implmentado em.close() nos repositorios
    
    * TarefaBean redirect
    
    * LoginBean logout() session.invalidate()
    
    * add C3-PO ao projeto

--- apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/LoginBean.java
+++ apptarefas/src/main/java/br/com/cursojava/apptarefas/usuario/LoginBean.java
@@ -44,8 +44,8 @@ public class LoginBean {
 		if(result.isOk()) {			
 			return "index.xhtml?faces-redirect=true";
 		}else{
-				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Inv·lido",
-					"Email ou senha inv·lidos");
+				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Inv√°lido",
+					"Email ou senha inv√°lidos");
 			ctx.addMessage(null, msg);
 			return null;
 		}
@@ -56,6 +56,7 @@ public class LoginBean {
 		HttpSession session = (HttpSession) ctx.getExternalContext().getSession(false);		
 			session.invalidate();
 			return "login.jsf?faces-redirect=true";		
+
 	}
 		
 }
